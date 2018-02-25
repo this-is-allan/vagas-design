74c410cc9d26e69824412dc5bdbb363245e4dbea
Allan Carlos <allancarlosdeveloper@gmail.com>
2018-02-25 08:33:22 -0300

    hide firebase config

---
+++ src/config/firebaseExample.js
@@ -0,0 +1,13 @@
+// export const firebase = {
+//     apiKey: "",
+//     authDomain: "",
+//     databaseURL: "",
+//     projectId: "",
+//     storageBucket: "",
+//     messagingSenderId: ""
+// };
+
+// export default { firebase };
+
+
+// ->>> Rename to firebase.js <<<-
