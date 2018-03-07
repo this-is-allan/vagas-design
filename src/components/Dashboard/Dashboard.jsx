938112cc2028e7c73ab79de38f3cd9e93a5dd9ac
Allan Carlos <allancarlosdeveloper@gmail.com>
2018-03-07 20:58:13 -0300

    Jobs Table in Dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { Grid, Card, Icon, Label } from 'semantic-ui-react'
+import { Grid, Table, Button, Checkbox, Card, Icon, Label } from 'semantic-ui-react'
 import faker from 'faker';
 import * as moment from 'moment';
 
@@ -24,8 +24,7 @@ class Dashboard extends Component {
 
         firestore.get({
             collection: 'jobs',
-            orderBy: ['createdAt', 'desc'],
-            where: ['status', '==', true]
+            orderBy: ['createdAt', 'desc']
         })
     }
 
@@ -33,25 +32,74 @@ class Dashboard extends Component {
         const { jobs } = this.props;
 
         return (
-            <Grid>
-                {_.map(jobs, job => {
-                    return (
-                        <Grid.Column mobile={16} computer={4} key={job.id}>
-                            <Card fluid>
-                                <Card.Content>
-                                    <Card.Header>{job.title.substr(0, 40)}</Card.Header>
-                                    <Card.Meta>{moment(job.createdAt).toNow(true)}</Card.Meta>
-                                    <Card.Description>
-                                        {job.remote ? <Label icon='globe' content='Remote' /> : ''}
-                                        {job.presential ? <Label icon='anchor' content='Presential' /> : ''}
-                                    </Card.Description>
-                                </Card.Content>
-                            </Card>
-                        </Grid.Column>
-                    );
-                })}
-            </Grid>
-        );
+            <Table compact celled definition>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell />
+                        <Table.HeaderCell>Title</Table.HeaderCell>
+                        <Table.HeaderCell>Created at</Table.HeaderCell>
+                        <Table.HeaderCell />
+                        <Table.HeaderCell />
+                    </Table.Row>
+                </Table.Header>
+
+                <Table.Body>
+                    {_.map(jobs, job => {
+                        return (
+                            <Table.Row key={job.id}>
+                                <Table.Cell collapsing>
+                                    <Checkbox slider defaultChecked={job.status} />
+                                </Table.Cell>
+                                <Table.Cell>{job.title.substring(0, 60)}...</Table.Cell>
+                                <Table.Cell>{moment(job.createdAt).format('MMMM Do YYYY, h:mm:ss a')}</Table.Cell>
+                                <Table.Cell>
+                                    {job.remote ? <Label icon='globe' content='Remote' /> : ''}
+                                    {job.presential ? <Label icon='anchor' content='Presential' /> : ''}
+                                </Table.Cell>
+                                <Table.Cell textAlign='center'>
+                                    <Button>Edit</Button>
+                                    <Button>Delete</Button>
+                                </Table.Cell>
+                            </Table.Row>
+                        )
+                    })}
+                </Table.Body>
+
+                <Table.Footer fullWidth>
+                    <Table.Row>
+                        <Table.HeaderCell />
+                        <Table.HeaderCell colSpan='4'>
+                            <Button floated='right' icon labelPosition='left' primary size='small'>
+                                <Icon name='user' /> Add User
+                            </Button>
+                            <Button size='small'>Approve</Button>
+                            <Button disabled size='small'>Approve All</Button>
+                        </Table.HeaderCell>
+                    </Table.Row>
+                </Table.Footer>
+            </Table>
+        )
+
+        // return (
+        //     <Grid>
+        //         {_.map(jobs, job => {
+        //             return (
+        //                 <Grid.Column mobile={16} computer={4} key={job.id}>
+        //                     <Card fluid>
+        //                         <Card.Content>
+        //                             <Card.Header>{job.title.substr(0, 40)}</Card.Header>
+        //                             <Card.Meta>{moment(job.createdAt).toNow(true)}</Card.Meta>
+        //                             <Card.Description>
+        //                                 {job.remote ? <Label icon='globe' content='Remote' /> : ''}
+        //                                 {job.presential ? <Label icon='anchor' content='Presential' /> : ''}
+        //                             </Card.Description>
+        //                         </Card.Content>
+        //                     </Card>
+        //                 </Grid.Column>
+        //             );
+        //         })}
+        //     </Grid>
+        // );
 
     }
 }
